cddd93719908b3f20ce6804823562001d0f04c61
Solomon Kebede <solomonaau188@gmail.com>
2022-11-08 18:05:17 +0000

    Update class initializer

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
+import uuid
+from datetime import datetime
 
 class BaseModel:
 	def __init__(self):
 		"""Instantiate a new object with a
 		unique id and the time of creation or
 		update"""
-		pass
+		self.id = str(uuid.uuid4())
+		self.created_at = datetime.now()
+		self.updated_at = datetime.now()
