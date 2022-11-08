992bf9487ce95954a8e257f283a2776682b9dc78
Solomon Kebede <solomonaau188@gmail.com>
2022-11-08 18:06:40 +0000

    Add str method

--- models/base_model.py
+++ models/base_model.py
@@ -11,3 +11,11 @@ class BaseModel:
 		self.id = str(uuid.uuid4())
 		self.created_at = datetime.now()
 		self.updated_at = datetime.now()
+
+
+	def __str__(self):
+		"""String method for object"""
+		return '[{}] ({}) {}'.format(
+			self.__class__.__name__,
+			self.id,
+			self.__dict__)
