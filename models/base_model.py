d7f1efedb8d2f4cccad73b74029075fa78799ba3
Solomon Kebede <solomonaau188@gmail.com>
2022-11-08 18:47:05 +0000

    Add to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -25,3 +25,17 @@ class BaseModel:
 		"""Update the public instance attrribute
 		updated_at with current datetime"""
 		self.updated_at = datetime.now()
+
+
+	def to_dict(self):
+		"""Returns a dictionary containing all
+		keys/values of __dict__ of the instance"""
+		new_dict = {}
+		for key, value in self.__dict__.items():
+			new_dict[key] = value
+		new_dict['__class__'] = self.__class__.__name__
+		new_dict['created_at'] = self.created_at.strftime(
+			'%Y-%m-%dT%H:%M:%S.%f')
+		new_dict['updated_at'] = self.updated_at.strftime(
+			'%Y-%m-%dT%H:%M:%S.%f')
+		return new_dict
